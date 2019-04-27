37a6b84fedef9619088180fd4576f9190b788c2f
Juan Pablo Garcia Gonzalez <jpgarcia69@hotmail.com>
2019-04-26 21:31:04 -0400

    demo sample code

---
+++ Logic-App-Demo/OrderTables.sql
@@ -0,0 +1,14 @@
+﻿
+CREATE TABLE [dbo].[OrderLines] (
+    [Id]                 INT           IDENTITY (1, 1) NOT NULL,
+    [gl_dimension_pkids] NVARCHAR (50) NULL,
+    [gl_dimension_code]  NVARCHAR (50) NULL,
+    [gl_code_reference]  NVARCHAR (50) NULL,
+    [gl_code]            NVARCHAR (50) NULL,
+    [item_code]          NVARCHAR (50) NULL,
+    [item_type]          NVARCHAR (50) NULL,
+    [qty]                INT           NOT NULL,
+    [unit_price]         INT           NOT NULL
+);
+
+
